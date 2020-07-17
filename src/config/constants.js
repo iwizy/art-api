23d7fe8c2bd803fb1d65a1676d81f1fcb03de221
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-07-17 23:25:04 +0300

    Constants config file and Mongo config

---
+++ src/config/constants.js
@@ -0,0 +1,28 @@
+const devConfig = {};
+const testConfig = {};
+const prodConfig = {};
+const defaultConfig = {
+  PORT: process.env.PORT || 3000,
+};
+
+function envConfig(env) {
+  switch (env) {
+    case 'development':
+      return devConfig;
+    case 'test':
+      return testConfig;
+    default:
+      return prodConfig;
+  }
+}
+
+//Take defaultConfig and make it a single object
+//So, we have concatenated two objects into one
+export default {
+  ...defaultConfig,
+  ...envConfig(process.env.NODE_ENV),
+};
+
+const devConfig = {MONGO_URL: 'mongodb://localhost/makeanodejsapi-dev',};
+const testConfig = {MONGO_URL: 'mongodb://localhost/makeanodejsapi-test',};
+const prodConfig = {MONGO_URL: 'mongodb://localhost/makeanodejsapi-prod',};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import express from 'express';
-
+import constants from './config/constants';
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, err => {
+let listen = app.listen(constants.PORT, err => {
   if (err) {
     throw err;
   } else {
-    console.log(
-      `Server running on port: ${PORT}
-      Running on ${process.env.NODE_ENV} 
-      visit: http://localhost:${PORT}`
-    );
+    console.log(`Server running on port: ${constants.PORT} --- Running on ${process.env.NODE_ENV} --- Make something great.!`)
   }
 });
