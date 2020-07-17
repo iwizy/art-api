1b8372d27bd18218d395e7ed84426467e138b02b
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-07-17 23:38:08 +0300

    Middleware setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import constants from './config/constants';
+import './config/database';
+import middlewareConfig from './config/middleware';
+
+
 const app = express();
+middlewareConfig(app);
 const PORT = process.env.PORT || 3000;
 let listen = app.listen(constants.PORT, err => {
   if (err) {
