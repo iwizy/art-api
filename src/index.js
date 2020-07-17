49c4de9febaf28abdb7d8be371b40c3e3f1f8bc2
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-07-18 00:10:36 +0300

    User: model, routers, validations

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import express from 'express';
 import constants from './config/constants';
 import './config/database';
 import middlewareConfig from './config/middleware';
-
+import apiRoutes from './modules';
 
 const app = express();
 middlewareConfig(app);
+app.get('/', (req, res) => {
+  res.json({
+    description: "REST API for artists collaborations",
+    author: "Alexander Agafonov",
+    version: "0.0.1"
+  });
+});
+apiRoutes(app);
 const PORT = process.env.PORT || 3000;
 let listen = app.listen(constants.PORT, err => {
   if (err) {
